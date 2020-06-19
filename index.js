cf948d2756c34e5f9bf04931dc888aa91f2b3e06
shritej <shritejmayekar@gmail.com>
2020-06-19 20:35:25 +0530

    Added bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static('public'))
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
